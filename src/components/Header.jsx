1c5f974c2df40f40aea1f734174595c9eecadb4a
iamshaswat <shaswatkumar281@bbdu.ac.in>
2024-04-20 15:07:27 +0530

    change

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,12 @@
 import React, { useState } from 'react'
-import profileImg from '../assets/Snapchat-214351935-01.jpeg'
 import resume from '../assets/fullstack.pdf'
 import { FiArrowDownCircle } from 'react-icons/fi';
 import Typewriter from 'typewriter-effect';
-
-import { useSwipeable } from 'react-swipeable';
+import { ProfileCard } from './ProfileCard';
 
 export const Header = () => {
 
-  const [profileCard, setProfileCard] = useState(false);
 
-
-  const handlers = useSwipeable({
-    onSwipeStart: () => setProfileCard(true),
-    onSwiped: () => setProfileCard(false)
-  })
 
   return (
     <div className='w-full'>
@@ -24,22 +16,7 @@ export const Header = () => {
         <div className='flex items-center justify-center text-center sm:w-1/2'>
 
           <div className='sm:w-1/2 w-auto '>
-
-            <div {...handlers} className={`flip-card ${profileCard ? 'flipped' : ''}`}>
-
-              <div className="flip-card-inner">
-
-                <div className="flip-card-front rounded-full">
-                  <img className='w-full ring-2 object-cover aspect-square rounded-full' src={'https://t4.ftcdn.net/jpg/05/90/45/35/360_F_590453560_ugMuPncnGYB6XnJqmC8xiPQx4eg3jmMD.jpg'} alt='profile' />
-                </div>
-
-                <div className="flip-card-back rounded-full items-center flex justify-center text-center">
-
-                  <img className='rounded-full object-cover aspect-square ring-2' src={profileImg} alt='profile' />
-                </div>
-
-              </div>
-            </div>
+            <ProfileCard />
 
             <p className='font-medium'>{'Hey !'}</p>
           </div>
