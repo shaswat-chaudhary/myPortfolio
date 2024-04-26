9a1fb40370a340be8345f8bf3b5657f3c7e19259
iamshaswat <shaswatkumar281@bbdu.ac.in>
2024-04-26 14:55:35 +0530

    change

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,29 +1,7 @@
-import React, { useEffect, useState } from 'react'
-import { Navbar } from '../components/Navbar'
-import Flippy, {BackSide, FrontSide} from 'react-flippy'
-
+import React from 'react'
 
 export const About = () => {
-
- const [profileCard, setProfileCard] = useState(false);
-
- const handleChangeCard = () => {
-  setProfileCard(!profileCard);
- }
-
   return (
-    <div>
-      <Navbar />
-      <div className={`flip-card ${profileCard ? 'flipped' : ''}`} onClick={handleChangeCard}>
-      <div className="flip-card-inner">
-        <div className="flip-card-front">
-          <p>{"frontContent"}</p>
-        </div>
-        <div className="flip-card-back">
-          <p>{"backContent"}</p>
-        </div>
-      </div>
-    </div>
-    </div>
+    <div>About</div>
   )
 }
